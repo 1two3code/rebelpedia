ec941cb62a2a03917c3d4fc3d661ea2f3269bf74
Hasse <mail@1two3code.com>
2023-02-14 15:17:23 +0100

    App is now Scrapyard.tsx

---
+++ code/scrapyard/src/components/Scrapyard/Scrapyard.tsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./Scrapyard.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Scrapyard</h1>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
--- code/scrapyard/src/index.tsx
+++ code/scrapyard/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import App from "./App";
+import Scrapyard from "./components/Scrapyard/Scrapyard";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Scrapyard />
   </React.StrictMode>
 );
 
