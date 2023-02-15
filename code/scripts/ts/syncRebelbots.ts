b860077042dc1cf3eb8673b67cc210bab3ffd23f
Hasse <mail@1two3code.com>
2023-02-15 09:58:05 +0100

    syncRebelbots now also downloads ability icons

--- code/scripts/ts/syncRebelbots.ts
+++ code/scripts/ts/syncRebelbots.ts
@@ -32,15 +32,26 @@ async function downloadImage(folder: string, uri: string) {
 }
 
 
-
-async function main() {
-  
-
-  // Download all abilities
+/**
+ * Download ability definitions and icons
+ */
+const stepAbilities = async () => {
   const abilities = await (await fetch('https://cdn.rebelbots.com/cards/keywords.json?refresh')).json() as any;
   await writeFile(join(dataFolder, 'abilities.json'), JSON.stringify(abilities))
 
-  // Download all passive skills
+  for (let i = 0; i < abilities.length; i++) {
+    const { iconURL } = abilities[i];
+    if (iconURL !== '') {
+      console.log(`Downloading ability icon ${basename(iconURL)}`)
+      await downloadImage(join(imageFolder, 'abilities'),iconURL)
+    }
+  }
+}
+
+/**
+ * Download passive skill definitions and icons
+ */
+const stepPassives = async () => {
   const [industrial, engineer, military] = [
     ['shielded', 'resistant', 'tactical', 'well-equipped'],
     ['aftermath', 'regeneration', 'agile', 'aggressive'],
@@ -48,17 +59,20 @@ async function main() {
   ];
   const passives = [...industrial, ...engineer, ...military];
 
-  // Download passive skills icons
   for (let i = 1; i < passives.length + 1; i++) {
     console.log(`Downloading passive skill icons ${i}`)
     await downloadImage(join(imageFolder, 'passives'), `https://cdn.rebelbots.com/cards/passiveskills/${passives[i]}.png`)
   }
+}
 
+/**
+ * Download Kingdom Card definitions and images
+ */
+const stepKingdomCards = async () => {  
   const numKingdomCards= 6;
   const kingdomCards = [];
   const kingdomCardsStartIndex = 400
 
-  // Download all Kingdom cards data
   for (let i = 1; i < numKingdomCards + 1; i++) {
     console.log(`Downloading Kingdom card ${i}`)
     const card = await (await fetch(`https://cdn.rebelbots.com/cards/${kingdomCardsStartIndex + i}.json`)).json() as any;
@@ -66,35 +80,45 @@ async function main() {
   }
   await writeFile(join(dataFolder, 'kingdomCards.json'), JSON.stringify(kingdomCards))
 
-  // Download all Kingdom card images
   for (let i = 0; i < kingdomCards.length; i++) {
     console.log(`Downloading kingdom card image ${basename(kingdomCards[i].cardURI)}`)
     await downloadImage(join(imageFolder, 'kingdomCards'), kingdomCards[i].cardURI)
   }
-  
+}
+
+/**
+ * Download Card definitions and images
+ */
+const stepCardsAndParts = async () => {
   const numCards = 300;
   const cards = [];
-  // Download all card data
+  
   for (let i = 1; i < numCards + 1; i++) {
     console.log(`Downloading card ${i}`)
     const card = await (await fetch(`https://cdn.rebelbots.com/cards/${i}.json`)).json() as any;
     cards.push(card);
   }
-
   await writeFile(join(dataFolder, 'cards.json'), JSON.stringify(cards))
 
-  // Download all part images
   const partUris = Array.from(new Set(cards.map(c => c.partURI)));
   for (let i = 0; i < partUris.length; i++) {
     console.log(`Downloading part image ${basename(partUris[i])}`)
     await downloadImage(join(imageFolder, 'parts'), partUris[i])
   }
 
-  // Download all card images
   for (let i = 0; i < cards.length; i++) {
     console.log(`Downloading card image ${basename(cards[i].cardURI)}`)
     await downloadImage(join(imageFolder, 'cards'), cards[i].cardURI)
   }
+}
+
+async function main() {
+  await stepAbilities();
+  // await stepPassives();
+  // await stepKingdomCards();
+  // await stepCardsAndParts();
+
+  console.log('Done!');
 }
 
 main();
