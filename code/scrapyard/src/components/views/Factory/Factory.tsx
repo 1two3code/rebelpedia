958071fb9e71d1f481d68fac40ef02c301cb84c1
Hasse <mail@1two3code.com>
2023-02-15 10:59:53 +0100

    Factory and Scrapyard changes responsibilities

--- code/scrapyard/src/components/views/Factory/Factory.tsx
+++ code/scrapyard/src/components/views/Factory/Factory.tsx
@@ -1,7 +1,8 @@
 import * as nftPolygon from "../../../assets/data/hasse.eth.json";
-import { assetToBot, InfuraAsset } from "../../../integrations/infura";
+import { assetToBot } from "../../../integrations/infura";
 import { botToDeck } from "../../../integrations/rebelbots";
 import { Deck } from "../../Deck/Deck";
+import { FighterBot } from "../../FighterBot/FighterBot";
 import "./Factory.scss";
 
 const figtherBotContract = "0x17892c8c3eb60c144872c18f013626471c3658bf";
@@ -9,18 +10,22 @@ const figtherBotContract = "0x17892c8c3eb60c144872c18f013626471c3658bf";
 const { assets } = nftPolygon;
 const botAssets = assets?.filter(
   (asset) => asset.contract === figtherBotContract
-) as InfuraAsset[];
-const bots = botAssets.map(assetToBot);
+);
+const bot = assetToBot(botAssets[0] as any);
 
 function Factory() {
   return (
     <div className="factory">
       <h1>Factory</h1>
-      <div>
-        {bots.map((bot) => (
-          <Deck key={bot.id} cards={botToDeck(bot)} />
-        ))}
-      </div>
+
+      {/* <div>Instructions and stuff here probably</div>
+      <input
+        type="text"
+        placeholder="Temporary until Web3 is integrated"
+        value="0xaaa3020504b41dfbfb04abffcbfe0e7eb83c6ed9"
+      /> */}
+      <FighterBot bot={bot} />
+      <Deck cards={botToDeck(bot)} />
     </div>
   );
 }
